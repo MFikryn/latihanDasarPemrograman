e72bb592635db6121a9d85726697845884a0b1a1
mfikry27 <147212328+MFikryn@users.noreply.github.com>
2024-03-09 15:24:15 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,17 @@
+# membuat variabel
+nama = "fikry"
+semester = 2
+kelas = "TI23B"   
+jurusan = "Teknik informatika"
+queto = "ngoding is my live"
+married = False
+adalahPerempuan = False
+hobi = ["motoran", "mancing"]
+
+print("Nama saya: ",nama)
+print("Semester: ",semester)
+print("Kelas : ",kelas)
+print("Jurusan : ",jurusan)
+print("Quetes saya : ",queto)
+print("sudah menikah : ",married)
+print("hobi : ",hobi)
